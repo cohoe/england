51ce451ecafa8398dbf824f29e9ab0368eb34beb
Grant Cohoe <grant@grantcohoe.com>
2020-04-05 23:24:25 -0400

    search ingredients

--- england/commands/search.py
+++ england/commands/search.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import logging
 from barbados.indexes import RecipeIndex
+from barbados.indexes import IngredientIndex
 
 
 class Search:
@@ -14,14 +15,17 @@ class Search:
 
         query_params = {
             'name_or_query': 'match_phrase',
-            'specs__ingredients__name': 'rum',
+            # 'specs__ingredients__name': 'rum',
+            'slug': 'rum',
         }
 
         # https://github.com/elastic/elasticsearch-dsl-py/issues/126
-        results = RecipeIndex.search()[0:1000].query(**query_params).sort('_score').execute()
+        # results = RecipeIndex.search()[0:1000].query(**query_params).sort('_score').execute()
+        results = IngredientIndex.search()[0:1000].query(**query_params).sort('_score').execute()
         logging.info("Got %s results." % results.hits.total.value)
         for hit in results:
             logging.info("%s :: %s" % (hit.slug, hit.meta.score))
+
 
     @staticmethod
     def _setup_args():
