c342b42f524eb176ca48a7860080dc3b1b5e129e
Grant Cohoe <grant@grantcohoe.com>
2020-04-18 20:54:44 -0400

    MixologyTech Models

--- england/commands/extract.py
+++ england/commands/extract.py
@@ -3,6 +3,8 @@ import sys
 import england.util
 import json
 from barbados.connectors.mixologytech import Database
+from barbados.connectors.sqlite import SqliteConnector
+from barbados.models.mixologytech import IngredientModel, IngredientAlternateSpellingModel
 
 
 class Extract:
@@ -13,81 +15,94 @@ class Extract:
         args = self._setup_args()
         self._validate_args(args)
 
-        d = Database(args.database)
+        dbconn = SqliteConnector(path=args.database)
 
-        # raw_ingredient = d.get_rows('ZINGREDIENT', 'Z_PK', 83)[0]
-        raw_ingredient = d.get_rows('ZINGREDIENT', 'ZCANONICALNAME', 'grape brandy')[0]
-
-        category_mappings = d.get_rows('Z_1CATEGORIES', 'Z_1INGREDIENTS1', raw_ingredient['Z_PK'])
-        # print(category_mappings)
-
-        raw_categories = []
-        for mapping in category_mappings:
-            category_pk = mapping['Z_3CATEGORIES']
-            raw_category = d.get_rows('ZINGREDIENTCATEGORY', 'Z_PK', category_pk)[0]
-            if raw_category['ZDISPLAYNAME'] is not None:
-                raw_categories.append(raw_category)
-        # results = d.get_rows('Z_1CATEGORIES', 'Z_1INGREDIENTS1', raw_ingredient['Z_PK'])
-
-        raw_synonyms = d.get_rows('ZINGREDIENTSYNONYM', 'ZINGREDIENT', raw_ingredient['Z_PK'])
-        raw_altnames = d.get_rows('ZINGREDIENTALTERNATESPELLING', 'ZINGREDIENT', raw_ingredient['Z_PK'])
-
-        details = self._parse_detail_json(raw_ingredient['ZDETAILJSON'])
-
-        template = england.util.load_template_from_file('./templates/ingredient.j2.yaml')
-
-        content = template.render(ingredient=raw_ingredient, synonyms=raw_synonyms, altnames=raw_altnames, details=details, categories=raw_categories)
-        print(content)
+        # print(len(IngredientModel.query.all()))
+        # for ingredient in IngredientModel.query.all():
+        #     print(ingredient.canonical_name)
+        for altname in IngredientAlternateSpellingModel.query.all():
+            print(altname.ingredient_id)
 
 
-        # print(raw_altnames)
-        # print(raw_synonyms)
-        # print(raw_categories)
-        # print(raw_ingredient)
+    # def run(self):
+    #     args = self._setup_args()
+    #     self._validate_args(args)
+    #
+    #     d = Database(args.database)
+    #
+    #     # raw_ingredient = d.get_rows('ZINGREDIENT', 'Z_PK', 83)[0]
+    #     raw_ingredient = d.get_rows('ZINGREDIENT', 'ZCANONICALNAME', 'grape brandy')[0]
+    #
+    #     category_mappings = d.get_rows('Z_1CATEGORIES', 'Z_1INGREDIENTS1', raw_ingredient['Z_PK'])
+    #     # print(category_mappings)
+    #
+    #     raw_categories = []
+    #     for mapping in category_mappings:
+    #         category_pk = mapping['Z_3CATEGORIES']
+    #         raw_category = d.get_rows('ZINGREDIENTCATEGORY', 'Z_PK', category_pk)[0]
+    #         if raw_category['ZDISPLAYNAME'] is not None:
+    #             raw_categories.append(raw_category)
+    #     # results = d.get_rows('Z_1CATEGORIES', 'Z_1INGREDIENTS1', raw_ingredient['Z_PK'])
+    #
+    #     raw_synonyms = d.get_rows('ZINGREDIENTSYNONYM', 'ZINGREDIENT', raw_ingredient['Z_PK'])
+    #     raw_altnames = d.get_rows('ZINGREDIENTALTERNATESPELLING', 'ZINGREDIENT', raw_ingredient['Z_PK'])
+    #
+    #     details = self._parse_detail_json(raw_ingredient['ZDETAILJSON'])
+    #
+    #     template = england.util.load_template_from_file('./templates/ingredient.j2.yaml')
+    #
+    #     content = template.render(ingredient=raw_ingredient, synonyms=raw_synonyms, altnames=raw_altnames, details=details, categories=raw_categories)
+    #     print(content)
+    #
+    #
+    #     # print(raw_altnames)
+    #     # print(raw_synonyms)
+    #     # print(raw_categories)
+    #     # print(raw_ingredient)
 
 
-    @staticmethod
-    def _parse_detail_json(detail_json):
-        raw_details = json.loads(detail_json)
-
-        details = {
-            'images': [],
-            'description': None,
-            'citations': [],
-            'origin': None,
-            'abv': None,
-            'substitute': None,
-            'avg_price_retail_us': None
-        }
-
-        for raw_detail in raw_details:
-            if raw_detail['kind'] == 'art':
-                for i in range(0,len(raw_detail['images'])):
-                    image = {
-                        'path': raw_detail['images'][i],
-                        'dims': raw_detail['dims'][i]
-                    }
-                    details['images'].append(image)
-            elif raw_detail['kind'] == 'ingDesc':
-                details['description'] = raw_detail['text']
-            elif raw_detail['kind'] == 'citation':
-                details['citations'].append({
-                    'text': raw_detail['text']
-                })
-            elif raw_detail['kind'] == 'tabular':
-                for row in raw_detail['rows']:
-                    if 'origin' in row[0].lower():
-                        details['origin'] = row[1]
-                    elif 'abv' in row[0].lower():
-                        details['abv'] = row[1]
-                    elif 'substitute' in row[0].lower():
-                        details['substitute'] = row[1]
-                    elif 'avg' in row[0].lower():
-                        details['avg_price_retail_us'] = row[1]
-            else:
-                print(raw_detail)
-
-        return details
+    # @staticmethod
+    # def _parse_detail_json(detail_json):
+    #     raw_details = json.loads(detail_json)
+    #
+    #     details = {
+    #         'images': [],
+    #         'description': None,
+    #         'citations': [],
+    #         'origin': None,
+    #         'abv': None,
+    #         'substitute': None,
+    #         'avg_price_retail_us': None
+    #     }
+    #
+    #     for raw_detail in raw_details:
+    #         if raw_detail['kind'] == 'art':
+    #             for i in range(0,len(raw_detail['images'])):
+    #                 image = {
+    #                     'path': raw_detail['images'][i],
+    #                     'dims': raw_detail['dims'][i]
+    #                 }
+    #                 details['images'].append(image)
+    #         elif raw_detail['kind'] == 'ingDesc':
+    #             details['description'] = raw_detail['text']
+    #         elif raw_detail['kind'] == 'citation':
+    #             details['citations'].append({
+    #                 'text': raw_detail['text']
+    #             })
+    #         elif raw_detail['kind'] == 'tabular':
+    #             for row in raw_detail['rows']:
+    #                 if 'origin' in row[0].lower():
+    #                     details['origin'] = row[1]
+    #                 elif 'abv' in row[0].lower():
+    #                     details['abv'] = row[1]
+    #                 elif 'substitute' in row[0].lower():
+    #                     details['substitute'] = row[1]
+    #                 elif 'avg' in row[0].lower():
+    #                     details['avg_price_retail_us'] = row[1]
+    #         else:
+    #             print(raw_detail)
+    #
+    #     return details
 
     @staticmethod
     def _setup_args():
