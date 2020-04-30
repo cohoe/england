e219980fedecf631d837bdc6cbeae9cefb6b1888
Grant Cohoe <grant@grantcohoe.com>
2020-04-30 01:10:47 -0400

    scraping works but import needs to deal with bogus ingredients

--- england/commands/scrape.py
+++ england/commands/scrape.py
@@ -5,6 +5,9 @@ from barbados.services import Registry
 from barbados.models import CocktailModel
 from barbados.serializers import ObjectSerializer
 from barbados.caches import CocktailScanCache
+from barbados.indexers import indexer_factory
+from pprint import pprint
+import ruamel.yaml
 
 
 class Scrape:
@@ -16,13 +19,24 @@ class Scrape:
         self._validate_args(args)
 
         pgconn = Registry.get_database_connection()
-        c = UpneatConnector.scrape_recipe(args.source)
+        # c = UpneatConnector.scrape_recipe(args.source)
+        #
+        # with pgconn.get_session() as session:
+        #     db_obj = CocktailModel(**ObjectSerializer.serialize(c, 'dict'))
+        #     session.add(db_obj)
+        #
+        objects = UpneatConnector.get_recipes()
+        # with pgconn.get_session() as session:
+        #     for c in objects:
+        #         db_obj = CocktailModel(**ObjectSerializer.serialize(c, 'dict'))
+        #         session.add(db_obj)
+        #         indexer_factory.get_indexer(c).index(c)
+        #
+        # CocktailScanCache.invalidate()
+        # for c in objects:
+        #     print(ruamel.yaml.round_trip_dump(ObjectSerializer.serialize(c, 'dict'), indent=2, block_seq_indent=3))
+        #     return
 
-        with pgconn.get_session() as session:
-            db_obj = CocktailModel(**ObjectSerializer.serialize(c, 'dict'))
-            session.add(db_obj)
-
-        CocktailScanCache.invalidate()
 
     @staticmethod
     def _setup_args():
